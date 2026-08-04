fabd9887fce6a08515f4172ad8167d5ba6459b77
Your Name <you@example.com>
2026-08-03 22:57:43 -0700

    file : adding some conext file

--- project/Torino/travel-agency-api/routes/tour.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tourController = require('../controllers/tourController');
-
-router.get('/', tourController.getTours);
-router.get('/:tourId', tourController.getTourById);
-
-module.exports = router;
