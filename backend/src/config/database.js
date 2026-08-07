6dfea1b43b051da8fd5e5633b79f92c721526b21
Your Name <you@example.com>
2026-08-07 12:52:28 +0300

    fix database connection

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME || "shopet_db",
-  user: process.env.DB_USER || "alfa",
-  password: process.env.DB_PASSWORD || "",
-  ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
